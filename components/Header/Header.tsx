7fba1a3ee3544078f3fce9faf33639f0997f4a8e
Muhammad Faizan Soomro <141871174+Faizan418@users.noreply.github.com>
2024-11-21 01:37:40 +0500

    update

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import styles from './Header.module.css'
-import { IoCloudDownloadOutline } from "react-icons/io5";
 
 const Header = () => {
   return (
@@ -10,9 +9,9 @@ const Header = () => {
           
           <div className={styles.div_one}>
 
-            <h3 className={styles.heading_first}>Hello! Everyone, I am</h3>
+            <h3 className={styles.heading_first}>Hello! Everyone&#39; I am</h3>
             <h1 className={styles.heading_second}>Muhammad Faizan</h1>
-            <p className={styles.heading_third}>I'm Student of Governor Sindh IT Initiative Cloud Computing Genrative <br /> AI and Metavarce and I'm Student of Saylani Mass IT Traning.</p>
+            <p className={styles.heading_third}>I&#39;m Student of Governor Sindh IT Initiative Cloud Computing Genrative <br /> AI and Metavarce and I&#39;m Student of Saylani Mass IT Traning.</p>
                 <div className={styles.btn_div}>
                   <button className={styles.btn1} type="button">Download CV </button>
                   <button className={styles.btn2} type="button">Explore More</button>
